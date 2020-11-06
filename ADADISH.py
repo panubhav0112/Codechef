013200943366a385137264b16c883f754a650800
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-11-06 20:12:13 +0530

    Create ADADISH.py

---
+++ ADADISH.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+  _ =input()
+  arr = list(map(int,input().split()))
+  sum1,sum2 = 0,0
+  for a in sorted(arr,reverse=True):
+    if sum1>sum2:
+      sum2+=a
+    else:
+      sum1+=a
+  print(max(sum1,sum2))
