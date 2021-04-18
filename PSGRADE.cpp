aae5a6b5757e352e38b50ae9ef235402afd43427
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2021-04-18 22:13:16 +0530

    Create PSGRADE.cpp

---
+++ PSGRADE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin>>t;
+  while(t--){
+    int am,bm,cm,tm,a,b,c;
+    cin>>am>>bm>>cm>>tm>>a>>b>>c;
+    if(a>=am && b>=bm && c>=cm && a+b+c>=tm) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+  }
+}
