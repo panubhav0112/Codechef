bc8c9ad8ccccfe9182cacf21730edfab758827b7
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2021-04-18 22:21:17 +0530

    Create CM164364.py

---
+++ CM164364.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+  n,x = list(map(int,input().split()))
+  arr = list(map(int,input().split()))
+  u = len(set(arr))
+  r = n-u
+  x-=r
+  if x>0:
+    u-=x
+  print(u)
